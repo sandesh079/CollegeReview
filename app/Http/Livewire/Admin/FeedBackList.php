fe45816339d62d2e28b79f94952292a4fb43d7e0
sandesh079 <sandesh.dahal022@gmail.com>
2022-08-06 11:21:17 +0545

    bipin

--- app/Http/Livewire/Admin/FeedBackList.php
+++ app/Http/Livewire/Admin/FeedBackList.php
@@ -9,10 +9,12 @@ class FeedBackList extends Component
 {
     public function render()
     {
-        $count  = DB::table('feed_back')->where('comment','like','%'.'good'.'%')->orWhere('comment','like','%'.'ok'.'%')->orWhere('comment','like','%'.'best'.'%')->orWhere('comment','like','%'.'superb'.'%')->orWhere('comment','like','%'.'outsanding'.'%')->count();
-        $total  = DB::table('feed_back')->count();
+        //$count  = DB::table('feed_back')->where('comment','like','%'.'good'.'%')->orWhere('comment','like','%'.'ok'.'%')->orWhere('comment','like','%'.'best'.'%')->orWhere('comment','like','%'.'superb'.'%')->orWhere('comment','like','%'.'outsanding'.'%')->count();
+        //$total  = DB::table('feed_back')->count();
        
-        $this->rating = ($count/$total)*100;
+
+        
+       // $this->rating = ($count/$total)*100;
         $feedBackLists = FeedBack::all();
         return view('livewire..admin.feed-back-list',["feedBackLists"=>$feedBackLists]);
     }
